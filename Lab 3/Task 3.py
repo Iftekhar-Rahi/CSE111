602abee8807672aa2eef589119aecf9401979cba
Rahi <iftekharhossainrahi@gmail.com>
2023-02-26 01:54:52 +0600

    Commit

--- Lab 3/Task 3.py
+++ Lab 3/Task 3.py
@@ -11,3 +11,13 @@ print('Pokemon 1:',team_pika.pokemon1_name, team_pika.pokemon1_power)
 print('Pokemon 2:',team_pika.pokemon2_name, team_pika.pokemon2_power)
 pika_combined_power = (team_pika.pokemon1_power + team_pika.pokemon2_power) * team_pika.damage_rate
 print('Combined Power:', pika_combined_power)
+team_bulb = Pokemon('bulbasaur', 'squirtle', 80, 70,
+9)
+print('=======Team 1=======')
+print('Pokemon 1:',team_bulb.pokemon1_name,
+team_bulb.pokemon1_power)
+print('Pokemon 2:',team_bulb.pokemon2_name,
+team_bulb.pokemon2_power)
+pika_combined_power = (team_bulb.pokemon1_power +
+team_bulb.pokemon2_power) * team_bulb.damage_rate
+print('Combined Power:', pika_combined_power)
