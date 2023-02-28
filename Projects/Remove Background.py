a7432473d59f80de919df9396523c9b3c9e5278c
Rahi <iftekharhossainrahi@gmail.com>
2023-03-01 00:34:06 +0600

    Commit

--- Projects/Remove Background.py
+++ Projects/Remove Background.py
@@ -1,7 +1,7 @@
 from rembg import remove
 from PIL import Image
-input_path=C:\Users\HP\Desktop\DSC02973.JPG
+input_path='C:\Users\HP\Desktop\DSC02973.JPG'
 image_input= Image.open(input_path)
 output=remove(image_input)
-output_path=C:\Users\HP\Desktop
+output_path='output.png'
 output.save(output_path)
