d43f6623b6b717222daca3ed216cd508e5f34410
Rahi <iftekharhossainrahi@gmail.com>
2023-02-28 10:10:02 +0600

    Tried Too hard to solve this problem

---
+++ Projects/Remove Background.py
@@ -0,0 +1,7 @@
+from rembg import remove
+from PIL import Image
+input_path=C:\Users\HP\Desktop\DSC02973.JPG
+image_input= Image.open(input_path)
+output=remove(image_input)
+output_path=C:\Users\HP\Desktop
+output.save(output_path)
