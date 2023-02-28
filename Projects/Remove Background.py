f765b4e2edf6fd79c201a1cf64f35dbbc9cbcd39
Rahi <iftekharhossainrahi@gmail.com>
2023-02-28 10:10:23 +0600

    Commit

--- Projects/Remove Background.py
+++ Projects/Remove Background.py
@@ -4,4 +4,4 @@ input_path=C:\Users\HP\Desktop\DSC02973.JPG
 image_input= Image.open(input_path)
 output=remove(image_input)
 output_path=C:\Users\HP\Desktop
-output.save(output_path)
+output.save(output_path
