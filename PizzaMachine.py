d29a1811b38f23fa00c6f4951a5694ff0279b656
Rahi <iftekharhossainrahi@gmail.com>
2023-03-03 13:08:06 +0600

    Commit

--- PizzaMachine.py
+++ PizzaMachine.py
@@ -1,35 +1,3 @@
-# class PizzaMachine:
-#     def __init__(self,*args):
-#         if len(args)==2:
-#             self.name=args[0]
-#             self.level=None
-#             self.inch=args[1]
-#         elif len(args)==1:
-#             self.name = args[0]
-#             self.level = None
-#             self.inch = 6
-#         elif len(args)==0:
-#             self.name = "Regular"
-#             self.level = "Hot"
-#             self.inch = 6
-#         self.all=["Regular","Hot","Super Naga"]
-#     def customizePizza(self,*args):
-#         if len(args)==2:
-#             self.toppings = args[0]
-#             self.level = args[1]
-#         elif len(args)==1:
-#             self.toppings = args[0]
-#         if type(self.toppings)!=list:
-#             return f"No toppings specified! Can't bake pizza."
-#         else:
-#             s=",".join(self.toppings)
-#             if self.level in self.all:
-#                 if len(self.toppings) == 0:
-#                     return f"No toppings specified! Can't bake pizza."
-#                 elif len(self.toppings) != 0:
-#                     return f"Your {self.inch}-inch {self.level} spicy {self.name} Pizza is ready with {s}. Enjoy!"
-#             else:
-#                 return f"Sorry! Spice level not allowed. Can't bake pizza."
 class PizzaMachine:
     def __init__(self,name="Regular",size=6):
         self.name=name
