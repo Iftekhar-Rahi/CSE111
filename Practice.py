76f39160b8f7b4e9e05d50cfeeda9e101c59d867
Rahi <iftekharhossainrahi@gmail.com>
2023-03-25 18:44:16 +0600

    Commit

--- Practice.py
+++ Practice.py
@@ -1,3 +1,10 @@
-a=[1,2,3,4,5,6]
-for i in a:
-    print("i")
+# t1 = ['T1-Express','New York','Manhattan','Brooklyn','Boston']
+# start="Manhattan"
+# end="Brooklyn"
+# fair=(t1.index(end)-t1.index(start))*100
+# print(fair)
+
+import random
+random_number = random.randint(1000, 9999)
+print(random_number)
+
