8e8aacf0c2972eaf33db161507d338a6d4919bb4
Rahi <iftekharhossainrahi@gmail.com>
2023-03-28 11:36:24 +0600

    Commit

--- 5 digit OTP.py
+++
@@ -1,3 +0,0 @@
-import random
-random_number = random.randint(1000, 9999)
-print(random_number)
--- Projects/Remove Background.py
+++
@@ -1,7 +0,0 @@
-from rembg import remove
-from PIL import Image
-input_path='C:\Users\HP\Desktop\DSC02973.JPG'
-image_input= Image.open(input_path)
-output=remove(image_input)
-output_path='output.png'
-output.save(output_path)
