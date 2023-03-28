2d6b34e4d2db7a5d8bafa6178c16759acbfd49eb
Rahi <iftekharhossainrahi@gmail.com>
2023-03-28 12:00:17 +0600

    Rearranged

--- LAB/Answers/Lab 2/Task 3.py
+++
@@ -1,7 +0,0 @@
-def add_divisor(min,max,div):
-    s=0
-    for i in range(min,max):
-        if i%div==0:
-            s+=i
-    return s
-print(add_divisor(3,16,3))
--- LAB/Answers/Lab 2/Task 8.py
+++
@@ -1,8 +0,0 @@
-#Task 8
-def counter(day):
-    year=day//365
-    year_remaining=day%365
-    month=year_remaining//30
-    days=year_remaining%30
-    print(f"{year} years, {month} months and {days} days")
-counter(4320)
